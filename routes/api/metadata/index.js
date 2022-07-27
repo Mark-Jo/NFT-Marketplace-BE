478980cfebde6f9d11398925b4f77d94a1ff25f8
Mark-JO <102936081+Mark-Jo@users.noreply.github.com>
2022-07-27 17:33:25 +0900

    Update index.js

--- routes/api/metadata/index.js
+++ routes/api/metadata/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const s3 = new AWS.S3({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: process.env.S3_RESION,
+  region: process.env.S3_REGION,
 });
 
 const upload = multer({
